43450817f5c70e86347f2a6c9fded990df10e90c
dave <dave@192.168.2.159>
2020-06-23 11:22:52 -0700

    ip working in serialization

--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -16,16 +16,19 @@ import javax.swing.JPanel;
 public class ToDoList implements ActionListener {
 	ArrayList<String> arr = new ArrayList<String>();
 	JFrame jf = new JFrame();
-	JPanel jp = new JPanel(); 
+	JPanel jp = new JPanel();
 	JButton jb = new JButton();
 	JButton jb1 = new JButton();
 	JButton jb2 = new JButton();
 	JButton jb3 = new JButton();
 	JButton jb4 = new JButton();
+
 	public static void main(String[] args) {
 		ToDoList tdl = new ToDoList();
 		tdl.setup();
-	}void setup(){
+	}
+
+	void setup() {
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		jf.setVisible(true);
@@ -35,78 +38,79 @@ public class ToDoList implements ActionListener {
 		jp.add(jb2);
 		jp.add(jb3);
 		jp.add(jb4);
-		
+
 		jb.setText("add task");
 		jb1.setText("View task");
 		jb2.setText("remove task");
 		jb3.setText("save list");
 		jb4.setText("load list");
-		
+
 		jb.addActionListener(this);
 		jb1.addActionListener(this);
 		jb2.addActionListener(this);
 		jb3.addActionListener(this);
 		jb4.addActionListener(this);
-		
-	jf.pack();	
+
+		jf.pack();
 	}
+
 	/*
-	 * Create a program with five buttons, add task, view tasks, remove task, save list, and load list. 
+	 * Create a program with five buttons, add task, view tasks, remove task, save
+	 * list, and load list.
 	 *
-	 * When add task is clicked:
-	 * 		Create a JOptionPane to ask the user for a task and add it to an ArrayList
+	 * When add task is clicked: Create a JOptionPane to ask the user for a task and
+	 * add it to an ArrayList
 	 * 
-	 * When the view tasks button is clicked:
-	 *		show all the tasks in the list
+	 * When the view tasks button is clicked: show all the tasks in the list
 	 * 
-	 * When the remove task button is clicked:
-	 * 		Create a JOptionPane to prompt the user for which task to remove and take it off of the list.
+	 * When the remove task button is clicked: Create a JOptionPane to prompt the
+	 * user for which task to remove and take it off of the list.
 	 * 
-	 * When the save list button is clicked:
-	 * 		Save the list to a file
+	 * When the save list button is clicked: Save the list to a file
 	 * 
-	 * When the load list button is clicked:
-	 * 		Create a JOptionPane to Prompt the user for the location of the file and load the list from that file
+	 * When the load list button is clicked: Create a JOptionPane to Prompt the user
+	 * for the location of the file and load the list from that file
 	 * 
-	 * When the program starts, it should automatically load the last saved file into the list. 
+	 * When the program starts, it should automatically load the last saved file
+	 * into the list.
 	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
-		if(e.getSource()==jb) {
+		if (e.getSource() == jb) {
 			String add = JOptionPane.showInputDialog("What is the task");
 			arr.add(add);
-		}else if(e.getSource()==jb1) {
-			if(arr.size()==0) {
+		} else if (e.getSource() == jb1) {
+			if (arr.size() == 0) {
 				JOptionPane.showMessageDialog(null, "You have no tasks");
 			}
 			for (int i = 0; i < arr.size(); i++) {
 				JOptionPane.showMessageDialog(null, arr.get(i));
 			}
-		}else if(e.getSource()==jb2) {
+		} else if (e.getSource() == jb2) {
 			String stringRemove = JOptionPane.showInputDialog("Which number task would you like to remove");
-			int remove = (Integer.parseInt(stringRemove)) -1;
-			
+			int remove = (Integer.parseInt(stringRemove)) - 1;
+
 			arr.remove(remove);
-		}else if(e.getSource()==jb3) {
+		} else if (e.getSource() == jb3) {
 			try {
 				FileWriter fw = new FileWriter("src/_00_Intro_To_File_Input_and_Output/test2.txt");
-				
-			
-				
-				fw.write(""+ arr);
-					
+
+				for (int i = 0; i < arr.size(); i++) {
+
+					fw.write(arr.get(i));
+				}
 				fw.close();
 			} catch (IOException arg0) {
 				arg0.printStackTrace();
 			}
-		}else if(e.getSource()==jb4) {
+		} else if (e.getSource() == jb4) {
 			String location = JOptionPane.showInputDialog("What is the location of the file");
 			try {
-				FileReader fr = new FileReader("src/_00_Intro_To_File_Input_and_Output/test.txt");
+				FileReader fr = new FileReader(location);
 				int c = fr.read();
-				while(c != -1){
-					System.out.print((char)c);
+				while (c != -1) {
+					System.out.print((char) c);
 					c = fr.read();
 				}
 				fr.close();
@@ -116,5 +120,5 @@ public class ToDoList implements ActionListener {
 				i.printStackTrace();
 			}
 		}
-		}
+	}
 }
